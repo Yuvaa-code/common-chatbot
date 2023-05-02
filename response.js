a8231339394efa0817d54a61915f941c5515391d
Yuvathangam <yuvaranit19@gmail.com>
2023-05-02 20:25:46 +0530

    common chatbot
    chatbot

---
+++ response.js
@@ -0,0 +1,13 @@
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  good:"nice how can i help you",
+  html:"HTML, or Hypertext Markup Language, is a markup language for the web that defines the structure of web pages.It is one of the most basic building blocks of every website, so it's crucial to learn if you want to have a career in web development.",
+  definition:"html   css   js   python  c  c++",
+  css:"CSS (Cascading Style Sheets) allows you to create great-looking web pages, but how does it work under the hood? This article explains what CSS is with a simple syntax example and also covers some key terms about the language.",
+  js:"JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc.",
+  python:"Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together. Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance.",
+  
+};
